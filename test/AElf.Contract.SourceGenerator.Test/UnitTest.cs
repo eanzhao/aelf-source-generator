64c03a8a686594391f2a3bd06d886dc773d3c5f3
eanzhao <yiqi.zhao@aelf.io>
2023-12-26 22:23:07 +0800

    Add test project.

--- test/AElf.Contract.SourceGenerator.Test/UnitTest.cs
+++ test/AElf.Contract.SourceGenerator.Test/UnitTest.cs
@@ -1,3 +1,8 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Shouldly;
+
 namespace AElf.Contract.SourceGenerator.Test;
 
 public class UnitTest
@@ -5,6 +10,26 @@ public class UnitTest
     [Fact]
     public void Test()
     {
-        
+        var filePath = $"{Environment.CurrentDirectory}/Protobuf/message/transaction_fee.proto";
+        var fileText = File.ReadAllText(filePath);
+        var output = GetGeneratedOutput(new TestAdditionalFile(filePath, fileText));
+        output.Count().ShouldBe(2);
+    }
+
+    private static IEnumerable<(string, string)> GetGeneratedOutput(params AdditionalText[] additionalFiles)
+    {
+        var compilation = CSharpCompilation.Create("ContractCodeGeneratorTests");
+        var generator = new ContractCodeGenerator();
+        CSharpGeneratorDriver.Create(generator)
+            .AddAdditionalTexts(additionalFiles.ToImmutableArray())
+            .RunGeneratorsAndUpdateCompilation(compilation,
+                out var outputCompilation,
+                out var diagnostics);
+
+        // optional
+        diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error)
+            .ShouldBeEmpty();
+
+        return outputCompilation.SyntaxTrees.Select(t => (t.FilePath.ToString(), t.GetText().ToString()));
     }
 }
